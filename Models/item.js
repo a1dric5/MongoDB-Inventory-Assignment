582a4588a1ae89ecef32c223ec036b3b71405888
Aldrich Lapid <aldrichlapid@gmail.com>
2023-06-29 23:21:18 -0700

    inventory app created

---
+++ Models/item.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose")
+
+const itemSchema = new mongoose.Schema({
+    price: Number,
+    inventory: Number,
+    nextDelivery: Date,
+    deliveryAmt: Number,
+    name: String
+})
+
+module.exports = mongoose.model("Item", itemSchema)
